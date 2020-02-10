d20f4286ccfa632cc87e02118995de94e403885e
FilipHalon <halon.filip@gmail.com>
2020-02-10 18:54:55 +0100

    axios nutritional data search prepared.

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import IngredientSearchInput from './IngredientSearchInput';
 
 const App = () => {
@@ -7,6 +8,24 @@ const App = () => {
     const handleSearch = e => {
         setSearchPhrase(e.target.value);
     }
+
+    useEffect(() => {
+        axios({
+            url: 'https://trackapi.nutritionix.com/v2/search/instant',
+            headers: {
+                'x-app-id': process.env.REACT_APP_NUTRITION_DB_API_ID,
+                'x-app-key': process.env.REACT_APP_NUTRITION_DB_API_KEY,
+                'x-remote-user-id': 0
+              },
+            params: {
+                query: searchPhrase,
+                self: false,
+                branded: false,
+                detailed: true,
+            }
+        })
+        .then(res => console.log(res))
+    }, [searchPhrase]);
 
     return (
         <IngredientSearchInput handleChange={handleSearch}/>
