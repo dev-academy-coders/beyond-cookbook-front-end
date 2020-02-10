af4c0b63ee58c79fd0b9313a4d85864fe315a4cc
FilipHalon <halon.filip@gmail.com>
2020-02-10 20:59:08 +0100

    Search choices displayed properly.

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import IngredientSearchInput from './IngredientSearchInput';
+import SearchChoicesSection from './SearchChoicesSection';
 
 const App = () => {
     const [searchPhrase, setSearchPhrase] = useState();
+    const [choices, setChoices] = useState();
 
     const handleSearch = e => {
         setSearchPhrase(e.target.value);
@@ -24,11 +26,17 @@ const App = () => {
                 detailed: true,
             }
         })
-        .then(res => console.log(res))
+        .then(res => {
+            console.log(res.data.common)
+            setChoices(res.data.common)
+        })
     }, [searchPhrase]);
 
     return (
-        <IngredientSearchInput handleChange={handleSearch}/>
+        <React.Fragment>
+            <IngredientSearchInput handleChange={handleSearch}/>
+            {choices && <SearchChoicesSection choices={choices}/>}
+        </React.Fragment>
     )
 }
 
---
+++ src/components/SearchChoiceButton.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const SearchChoiceButton = props => {
+    return <button>{props.name}</button>
+};
+
+export default SearchChoiceButton;
